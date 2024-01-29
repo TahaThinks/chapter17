24a0391108c624b350b6278df915e62d885b7518
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-01-29 20:22:00 +0400

    Print GET Request Response

--- hn-article.py
+++ hn-article.py
@@ -1,3 +1,7 @@
 import requests
 import json
 
+#Make an API call, and store the response/
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status Code: {r.status_code}")
