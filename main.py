d53b6b02bf99973c938dd77e2e1250ef4d35778c
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-01-29 19:48:57 +0400

    Most-Starred Python Projects on GitHub as of 29/01/2024

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+from plotly.graph_objs import Bar
+from plotly import offline
+
 #Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {"Accept":"application/vnd.github.v3+json"}
@@ -8,19 +11,26 @@ print(f"Status code: {r.status_code}")
 
 #Store API response in a variable.
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
-
 #Explore information about the repositories
 repo_dicts = response_dict['items']
-print(f"repositories returned: {len(repo_dicts)}")
 
-#Examine the first repository
-print("\nSelected information about each repository:")
+repo_names, stars = [], []
 for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f"Description: {repo_dict['description']}")
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+#Make Visulaization
+data = [{
+    'type':'bar',
+    'x':repo_names,
+    'y':stars,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub [TahaLearns]',
+    'xaxis': {'title':'Repository'},
+    'yaxis': {'title':'Stars'},
+}
+
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig, filename='python_repos.html')
