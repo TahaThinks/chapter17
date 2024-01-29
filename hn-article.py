ac51cc142cbd878c3568b0f57bd83579f966b52d
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-01-29 20:53:43 +0400

    Get most commented articles

--- hn-article.py
+++ hn-article.py
@@ -1,13 +1,34 @@
 import requests
-import json
+from operator import itemgetter
 
-#Make an API call, and store the response/
-url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+#Make an API call, and store the response
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status Code: {r.status_code}")
 
-#Explore the structure of the data
-response_dict = r.json()
-readable_file = 'data/readable_hn_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(response_dict, f, indent=4)
+#Process the information about rach submission.
+submission_ids = r.json()
+submission_dicts = []
+
+
+for submission_id in submission_ids[:5]:
+    #Make a seperate API call for each submission.
+    url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    #Build a dictionary for each article.
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link':f"https://news.ycombinator.com/item?id={submission_id}",
+        'comments':response_dict['descendants'],
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"\nDiscussion: {submission_dict['hn_link']}")
+    print(f"\nComments: {submission_dict['comments']}")
