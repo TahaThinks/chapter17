a6e43dabdbdccb86fde0c1fb5efa7f6388c60316
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-01-29 20:07:29 +0400

    Replace Xvalues with URLs for the Repo

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ response_dict = r.json()
 #Explore information about the repositories
 repo_dicts = response_dict['items']
 
-repo_names, stars, labels = [], [], []
+repo_links, stars, labels = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href = '{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    
     stars.append(repo_dict['stargazers_count'])
+    
     owner = repo_dict['owner']['login']
     description = repo_dict['description']
     label = f"{owner}<br />{description}"
@@ -26,7 +32,7 @@ for repo_dict in repo_dicts:
 #Make Visulaization
 data = [{
     'type':'bar',
-    'x':repo_names,
+    'x':repo_links,
     'y':stars,
     'hovertext': labels,
     'marker':{
